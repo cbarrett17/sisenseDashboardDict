5a165147b5306798a7f23c03067375587c884b4e
Doomslug <cbarrett17@georgefox.edu>
2022-10-05 12:54:44 -0700

    Framework of the program and initial value titles added to table

--- src/com/company/SDDict.java
+++ src/com/company/SDDict.java
@@ -1,5 +1,24 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class SDDict {
-    // Your class goes here
+
+    public static HashMap<String, String> descLookup;
+
+    public static void main(String[] args) {
+        // main method
+        // nerve center of the program
+    }
+
+    private static HashMap<String, String> descLookup() {
+        // creates the appropriate hashMap for use
+        // calls selectFile
+    }
+
+    private static Scanner selectFile() {
+        // selects which table to populate the hashMap from
+    }
 }
